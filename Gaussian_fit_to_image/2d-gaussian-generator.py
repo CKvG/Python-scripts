cfc4d44ec27bd099a6031ff93690b5de3ea672d3
CKvG <chris.grundherr@gmail.com>
2020-12-22 11:03:42 +0100

    Create 2d-gaussian-generator.py

---
+++ Gaussian_fit_to_image/2d-gaussian-generator.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 30 09:49:53 2020
+
+@author: CKvG
+"""
+
+
+import scipy.optimize as opt
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Define if image should have noise
+noisy = True
+noise_amp = 0.005
+
+def twoD_Gaussian(data, amplitude, xo, yo, sigma_x, sigma_y, theta, offset):
+    x, y = data
+    xo = float(xo)
+    yo = float(yo)    
+    a = (np.cos(theta)**2)/(2*sigma_x**2) + (np.sin(theta)**2)/(2*sigma_y**2)
+    b = -(np.sin(2*theta))/(4*sigma_x**2) + (np.sin(2*theta))/(4*sigma_y**2)
+    c = (np.sin(theta)**2)/(2*sigma_x**2) + (np.cos(theta)**2)/(2*sigma_y**2)
+    g = offset + amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo)
+                            + c*((y-yo)**2)))
+    return g.ravel()
+
+# Create x and y indices/ Size of picture
+x = np.linspace(0, 1280, 1281)
+y = np.linspace(0, 1024, 1025)
+x, y = np.meshgrid(x, y)
+data = (x,y)
+
+#create data
+image_data = twoD_Gaussian(data, 0.38, 518, 621, 154, 153, 0, 0.12)
+if noisy: image_data = image_data + noise_amp*np.random.normal(size=image_data.shape)
+
+# plot twoD_Gaussian data generated above
+fig = plt.figure(frameon=False, figsize=(12.80, 10.24), dpi=100)
+ax = plt.Axes(fig, [0., 0., 1., 1.])
+ax.set_axis_off()
+fig.add_axes(ax)
+ax.imshow(image_data.reshape(1025,1281), cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
+
+fig.savefig('Gaussian_Picture.png', cmap=plt.get_cmap('gray'))
